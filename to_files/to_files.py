e9f4246b208204dc4f633865f66f96f9151bf7b7
Alex Star <staritsyn@mail.ru>
2025-06-17 21:32:57 +0700

    Чуть изменил модуль работы с файлом, где запись в файл

--- to_files/to_files.py
+++ to_files/to_files.py
@@ -1,8 +1,12 @@
+from io import TextIOWrapper
+
 def write_to_file_message(path: str, message):
-    f = open(path, 'a')
+    f: TextIOWrapper
     try:
-        #print(dtn.strftime("%d-%m-%Y %H:%M"), '\tПользователь \t' + message.from_user.first_name, '\t' + str(message.from_user.username), '\t' + str(message.from_user.last_name), '\t' + str(message.from_user.id), '\tнаписал следующее: \t' + message.text + '\t', message.contact, file=f)
+        f = open(path, 'a')
         print(message, file=f)
-    except Exception:
-        print('Любая ошибка!', file=f)
+    except Exception as e:
+        print(e, file=f)
     f.close()
+
+# print(dtn.strftime("%d-%m-%Y %H:%M"), '\tПользователь \t' + message.from_user.first_name, '\t' + str(message.from_user.username), '\t' + str(message.from_user.last_name), '\t' + str(message.from_user.id), '\tнаписал следующее: \t' + message.text + '\t', message.contact, file=f)
